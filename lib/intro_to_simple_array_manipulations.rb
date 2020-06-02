2cacb40a6b80745d9472657fa8059eae7253c1f1
Hyung Kim <hjkmines@gmail.com>
2020-06-02 00:44:48 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,16 @@
+def using_concat (array1, array2)
+  combined_array = array1.concat(array2)
+  combined_array
+end
+
+def using_insert (array, element)
+  
+  array.insert(4, element)
+  
+end
+
+def using_uniq (array)
+  
+  
+  
+end
